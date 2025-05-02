02aa2189917ced0850e7b3d652136d474cc73c2b
JosephRan <JosephRangel@csu.fullerton.edu>
2025-05-02 10:53:51 -0700

    test

--- cooking-app/src/components/NavBar.jsx
+++ cooking-app/src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import LoginButton from './GoLoginButton';
 
 export default function NavBar() {
   const location = useLocation();
