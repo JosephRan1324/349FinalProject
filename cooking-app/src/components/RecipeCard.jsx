839e490e4baeed1083ecf26a8cfd20b20b3bb00d
JosephRan <JosephRangel@csu.fullerton.edu>
2025-05-02 11:17:33 -0700

    Recipecard ingredients

--- cooking-app/src/components/RecipeCard.jsx
+++ cooking-app/src/components/RecipeCard.jsx
@@ -1,3 +1,4 @@
+// src/components/RecipeCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -5,8 +6,20 @@ export default function RecipeCard({ recipe }) {
   return (
     <Link to={`/recipe/${recipe.id}`} className="card">
       <img src={recipe.image} alt={recipe.title} />
-      <h3>{recipe.title}</h3>
-      <p>{recipe.prepTime} | {recipe.difficulty}</p>
+
+      <div className="card-content">
+        <h3>{recipe.title}</h3>
+        <p>{recipe.prepTime} | {recipe.difficulty}</p>
+        
+        <div className="ingredients-preview">
+          <h4>Ingredients:</h4>
+          <ul>
+            {recipe.ingredients.map((ing, i) => (
+              <li key={i}>{ing}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </Link>
   );
 }
