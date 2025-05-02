2a761b270af411bb9e648136de000814344875a9
JosephRan <JosephRangel@csu.fullerton.edu>
2025-05-02 11:19:37 -0700

    Undo

--- cooking-app/src/components/RecipeCard.jsx
+++ cooking-app/src/components/RecipeCard.jsx
@@ -1,4 +1,3 @@
-// src/components/RecipeCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -6,20 +5,8 @@ export default function RecipeCard({ recipe }) {
   return (
     <Link to={`/recipe/${recipe.id}`} className="card">
       <img src={recipe.image} alt={recipe.title} />
-
-      <div className="card-content">
-        <h3>{recipe.title}</h3>
-        <p>{recipe.prepTime} | {recipe.difficulty}</p>
-        
-        <div className="ingredients-preview">
-          <h4>Ingredients:</h4>
-          <ul>
-            {recipe.ingredients.map((ing, i) => (
-              <li key={i}>{ing}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <h3>{recipe.title}</h3>
+      <p>{recipe.prepTime} | {recipe.difficulty}</p>
     </Link>
   );
 }
